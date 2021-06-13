7b6cbc889858851c60d26e1e21c2b97e84f10392
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-13 05:58:53 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# This Programm write by Mr.nope
+clear
+echo "Installing..."
+sleep 2
+sudo apt install python
+sudo apt install python3
+chmod a+x run.pyc
+sudo cp run.pyc /usr/lib/python3.6
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+exit 1
---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This Programm write by Mr.nope
+clear
+echo "Uninstalling..."
+sleep 2
+cd /usr/lib/python3.6 && sudo rm run.pyc
+echo ""
+echo "Uninstalling..., Finish...!"
+echo ""
+exit 1
